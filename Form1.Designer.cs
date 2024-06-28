ea346c82ce747ca282216a85d011c16a8dc43688
AlexGabrielSilveira <alexgabrielyou@gmail.com>
2024-06-28 02:09:29 -0300

    player criado com função de abrir arquivo e toca-lo

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.UPlayList = new System.Windows.Forms.ListBox();
+            this.Open = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player
+            // 
+            this.Player.Enabled = true;
+            this.Player.Location = new System.Drawing.Point(12, 12);
+            this.Player.Name = "Player";
+            this.Player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Player.OcxState")));
+            this.Player.Size = new System.Drawing.Size(863, 468);
+            this.Player.TabIndex = 0;
+            // 
+            // UPlayList
+            // 
+            this.UPlayList.FormattingEnabled = true;
+            this.UPlayList.Location = new System.Drawing.Point(881, 12);
+            this.UPlayList.Name = "UPlayList";
+            this.UPlayList.Size = new System.Drawing.Size(188, 472);
+            this.UPlayList.TabIndex = 1;
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(890, 448);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(78, 23);
+            this.Open.TabIndex = 2;
+            this.Open.Text = "Open";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(978, 448);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(78, 23);
+            this.Save.TabIndex = 3;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackgroundImage = global::player.Properties.Resources.angra;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1079, 495);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Open);
+            this.Controls.Add(this.UPlayList);
+            this.Controls.Add(this.Player);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Player";
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer Player;
+        private System.Windows.Forms.ListBox UPlayList;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace player
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        WMPLib.IWMPPlaylist playlist;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog.Title = "Open";
+            openFileDialog.Filter = "Arquivo mp3|*.mp3|arquivo wav| *.wav|arquivo m4a| *.m4a|arquivo aac| *.aac"; 
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK) 
+            {
+                playlist = Player.playlistCollection.newPlaylist("List");
+
+                foreach (var file in openFileDialog.FileNames)
+                {
+                    playlist.appendItem(Player.newMedia(file));
+                    UPlayList.Items.Add(file);
+
+                    Player.currentPlaylist = playlist;
+                    Player.Ctlcontrols.play();
+                }
+            }
+        }   
+    }
+}
